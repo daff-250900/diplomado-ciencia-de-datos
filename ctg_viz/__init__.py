8f48934c6da8a5208545bbdce6bfc71739a873be
daff-250900 <dafnegonzalez09@gmail.com>
2025-11-25 20:16:10 -0600

    tipado estatico

--- ctg_viz/__init__.py
+++ ctg_viz/__init__.py
@@ -1,21 +1,28 @@
 from . import preprocessing
+import pandas as pd
 
 class CTGViz:
     """
     Clase principal para visualización de datos CTG.
     """
     
-    def __init__(self, dataframe):
+    def __init__(self, dataframe: pd.DataFrame) -> None:
         """
         Inicializa la clase con un DataFrame.
         
         :param dataframe: pd.DataFrame, el DataFrame a analizar.
         """
-        self.df = dataframe
+        self.df: pd.DataFrame = dataframe
     
-    def check_data_completeness_dafneelizabethzepedagonzalez(self):
+    def check_data_completeness_dafneelizabethzepedagonzalez(self) -> None:
         """
         Analiza la completitud de los datos.
         """
         return preprocessing.check_data_completeness(self)
+
+    def vars_classification(self) -> tuple[list[str], list[str]]:
+        """
+        Clasifica las variables en discretas y continuas.
+        """
+        return preprocessing.vars_clasification(self)
     
--- ctg_viz/preprocessing.py
+++ ctg_viz/preprocessing.py
@@ -11,13 +11,13 @@ def check_data_completeness(self):
 
     """
     # Capturar el output en un string
-    output = io.StringIO()
+    output: io.StringIO = io.StringIO()
     
     # Calcula la cantidad de datos faltantes por columna
     total_missing = self.df.isnull().sum()
 
     # Calcula el porcentaje de datos faltantes
-    total_cells = self.df.size
+    total_cells: int = self.df.size
     percentage_missing = (total_missing / total_cells) * 100
 
     # Escribir los resultados en el buffer
@@ -31,13 +31,13 @@ def check_data_completeness(self):
     output.write("---------------------------------------------------------\n\n")
     
     # Obtener el contenido como string
-    content = output.getvalue()
+    content: str = output.getvalue()
     output.close()
     
     # Escapar HTML y crear contenedor con scroll
-    escaped_content = html.escape(content)
+    escaped_content: str = html.escape(content)
     
-    html_content = f"""
+    html_content: str = f"""
     <div style="
         max-height: 500px;
         overflow-y: auto;
@@ -58,3 +58,20 @@ def check_data_completeness(self):
     
     # Mostrar el contenido con scroll
     display(HTML(html_content))
+
+
+def vars_clasification(self):
+    """
+    Clasifica las variables en discretas y continuas.
+    """
+
+    varc: list = []
+    vard: list = []
+    
+    for col in self.df.columns:
+        if self.df[col].dtype == 'int64' or self.df[col].dtype == 'float64' and self.df[col].nunique() > 10:
+            varc.append(col)
+        else:
+            vard.append(col)
+
+    return varc, vard
